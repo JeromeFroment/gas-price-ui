794b2e25f27f49ec77339ee14a01a6dc942c5b10
Jérôme Froment <jerome.froment@xn--tu-7fu.univ-cotedazur.fr>
2022-02-27 16:53:50 +0100

    filter limit

--- src/components/model/filterModel.js
+++ src/components/model/filterModel.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 export class FilterModel {
+    limit;
     road;
     price;
     fuel;
 
-    constructor(road = null, price = null, fuel = null) {
+    constructor(limit = null, road = null, price = null, fuel = null) {
+        this.setLimit(limit);
         this.setRoad(road);
         this.setPrice(price);
         this.setFuel(fuel);
+    }
+
+    setLimit(limit = null) {
+        if(limit <= 0) { limit = null; }
+        this.limit = limit;
     }
 
     setRoad(road = null) {
@@ -27,6 +34,7 @@ export class FilterModel {
     }
 
     checkFilters() {
+        this.setLimit(this.limit);
         this.setRoad(this.road);
         this.setPrice(this.price);
         this.setFuel(this.fuel);
